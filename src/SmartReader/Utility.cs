9267cfaa6345a3de880ea19b946c349be098f3cc
Jason Nelson <jason@carbonmade.com>
2021-06-01 17:31:15 -0700

    Simplify SetNodeTag

--- src/SmartReader/Utility.cs
+++ src/SmartReader/Utility.cs
@@ -63,9 +63,13 @@ namespace SmartReader
 
             for (var i = 0; i < node.Attributes.Length; i++)
             {
+                var attr = node.Attributes[i];
+
                 // the possible result of malformed HTML
-                if (!node.Attributes[i].Name.Contains("<") && !node.Attributes[i].Name.Contains(">"))
-                    replacement.SetAttribute(node.Attributes[i].Name, node.Attributes[i].Value);
+                if (!attr.Name.Contains("<") && !attr.Name.Contains(">"))
+                {
+                    replacement.SetAttribute(attr.Name, attr.Value);
+                }
             }
             return replacement;
         }
