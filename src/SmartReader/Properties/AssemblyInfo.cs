09e3c3b1e1e859a69b5fa38a67286da61be86de1
Jason Nelson <jason@carbonmade.com>
2021-06-03 17:51:16 -0700

    Move various assembly properties to csproj

--- src/SmartReader/Properties/AssemblyInfo.cs
+++ src/SmartReader/Properties/AssemblyInfo.cs
@@ -1,17 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SmartReader")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Strumenta")]
 [assembly: AssemblyProduct("SmartReader")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyDescription("A .NET Standard library to extract the main content of a web page based on a port of the Readability library by Mozilla. It also determine and gather metadata about the content, such as language, author, main image, etc.")]
-[assembly: AssemblyVersion("0.7.5")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
