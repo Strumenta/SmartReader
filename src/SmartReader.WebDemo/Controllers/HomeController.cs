12970e92d2b4ddb61ffc165b80560c565c420946
gabriele-tomassetti <gabriele@strumenta.com>
2023-02-03 21:41:11 +0100

    Update dependencies

--- src/SmartReader.WebDemo/Controllers/HomeController.cs
+++ src/SmartReader.WebDemo/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Ganss.XSS;
+using Ganss.Xss;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SmartReader.WebDemo.Models;
